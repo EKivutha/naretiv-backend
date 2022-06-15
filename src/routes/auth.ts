bb8a97234054028d92427589b1eac4af6cd39d2f
Ezra <gerkibz@outlook.com>
2022-06-15 15:46:03 +0300

    add user routes

---
+++ src/routes/auth.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import { registerUserHandler, loginUserHandler, logoutHandler, refreshAccessTokenHandler } from '../controller/auth';
+import { deserializeUser } from '../middleware/deserializeUser';
+import { requireUser } from '../middleware/requireUser';
+import { validate } from '../middleware/validate';
+import { createUserSchema, loginUserSchema } from '../schemas/user';
+
+const router = express.Router();
+
+// Register user
+router.post('/register/', validate(createUserSchema), registerUserHandler);
+
+// Login user
+router.post('/login', validate(loginUserSchema), loginUserHandler);
+
+// Logout user
+router.get('/logout', deserializeUser, requireUser, logoutHandler);
+
+// Refresh access token
+router.get('/refresh', refreshAccessTokenHandler);
+
+export default router;
+
