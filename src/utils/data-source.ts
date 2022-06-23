dc38e7cb751a673ae1194750c5d1b2ec6315f200
Ezra <gerkibz@outlook.com>
2022-06-23 10:40:40 +0300

    update utils datasource file

--- src/utils/data-source.ts
+++ src/utils/data-source.ts
@@ -3,6 +3,14 @@ require('dotenv').config()
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import config from 'config';
+import { Message } from '../entity/message';
+import { Order } from '../entity/order';
+import { Payment } from '../entity/payments';
+import { Product } from '../entity/product';
+import { Rider } from '../entity/rider';
+import { Shipment } from '../entity/shipment';
+import { User } from '../entity/user';
+import { CreateAdminUser1547919837483 } from '../migration/createAdminUser';
 
 const postgresConfig = config.get<{
   host: string;
@@ -15,10 +23,10 @@ const postgresConfig = config.get<{
 export const AppDataSource = new DataSource({
   ...postgresConfig,
   type: 'postgres',
-  synchronize: false,
+  synchronize: true, //allow message and auto order updates through api subscriptions.
   logging: false,
-  entities: ['src/entity/**/*.entity{.ts,.js}'],
-  migrations: ['src/migration/**/*{.ts,.js}'],
+  entities: [User, Product, Message, Order, Payment, Shipment, Rider],
+  migrations: [CreateAdminUser1547919837483],
   subscribers: ['src/subscribers/**/*{.ts,.js}'],
 });
 
