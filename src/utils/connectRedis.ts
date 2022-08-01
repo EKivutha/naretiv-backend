a75318dbcc322be0a9e320715cace361c977e2bd
Ezra <gerkibz@outlook.com>
2022-08-01 19:03:05 +0300

    update redis socket host

--- src/utils/connectRedis.ts
+++ src/utils/connectRedis.ts
@@ -3,7 +3,10 @@ import { createClient } from 'redis';
 const redisUrl = 'redis://localhost:6379';
 
 const redisClient = createClient({
-  url: redisUrl,
+  socket:{
+    host: 'redis',
+    port: 6379
+  }
 });
 
 const connectRedis = async () => {
