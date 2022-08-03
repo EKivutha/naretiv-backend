87c97a99f0ddeff809a24436c06e74615c0bf472
Ezra <gerkibz@outlook.com>
2022-08-03 18:32:51 +0300

    update origin

--- config/default.ts
+++ config/default.ts
@@ -4,7 +4,7 @@ import config from "config";
 
 // const port = config.get<number>('port');
 export default {
-    origin: `http://localhost:8000`,
+    origin: `http://44.202.32.175:8000`,
     accessTokenExpiresIn: 15,
     refreshTokenExpiresIn: 60,
     redisCacheExpiresIn: 60,
